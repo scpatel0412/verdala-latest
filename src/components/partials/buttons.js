a57e4832633799e8a1b4187f9a2affaaae24c328
ShubhamAnchliya1 <shubham.anchliya@iflair.com>
2023-05-05 11:39:13 +0530

    button undo

--- src/components/partials/buttons.js
+++ src/components/partials/buttons.js
@@ -1,56 +1,20 @@
-// import { Link } from "gatsby";
-// import React from "react"
-// import ButtonArrow from '../../assets/svgs/button-arrow.svg'
-
-// const Button = ({ style, link, text,classname }) => (
-//     <Link to={link} className={classname?classname:"mainbtn"}>
-//         {text}
-//         {/* <div className={"button " + style}>
-//             <div className="inner-button">
-//                 <span>{text}</span>
-
-//                 <div className="button-arrow">
-//                     <ButtonArrow />
-//                 </div>
-//             </div>
-//         </div> */}
-//     </Link>
-// )
-
-// export default Button;
-
 import { Link } from "gatsby";
 import React from "react";
 import ButtonArrow from "../../assets/svgs/button-arrow.svg";
 
-const Button = (props) => {
-  return (
-    <>
-      {Object.prototype.hasOwnProperty.call(props, "hello") ? (
-        <div onClick={() => props.onClick()}>{props.text}</div>
-      ) : (
-        <Link
-          to={props.link}
-          className={props.classname ? props.classname : "mainbtn"}
-        >
-          {props.text}
+const Button = ({ style, link, text, classname }) => (
+  <Link to={link} className={classname ? classname : "mainbtn"}>
+    {text}
+    {/* <div className={"button " + style}>
+            <div className="inner-button">
+                <span>{text}</span>
 
-          <div className="button-arrow">
-            <ButtonArrow />
-          </div>
-          {/* <div className={"button " + style}>
-        <div className="inner-button">
-            <span>{text}</span>
-            
-            <div className="button-arrow">
-                <ButtonArrow />
+                <div className="button-arrow">
+                    <ButtonArrow />
+                </div>
             </div>
-        </div>
-    </div> */}
-        </Link>
-      )}
-    </>
-  );
-};
+        </div> */}
+  </Link>
+);
 
 export default Button;
